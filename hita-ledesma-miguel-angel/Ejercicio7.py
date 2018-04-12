eaa78776d5a2e245b71b0df3d0722aace22968f6
migue64 <migueangelhita63@gmail.com>
2018-04-12 11:19:43 +0200

    Ejercicio7

---
+++ hita-ledesma-miguel-angel/Ejercicio7.py
@@ -0,0 +1,11 @@
+import time
+palabra=0
+var1="s"
+lista=[]
+while var1=="s":
+	palabra=input("dime una palabra:\n")
+	var1=input("¿Quieres seguir?(s/n):\n")
+	lista.append("palabra")
+	for i in lista:
+		print (1)
+		time.sleep(0.65)
